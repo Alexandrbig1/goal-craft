5cac35d4d18893c4a5d4179b60fd490c019a7115
Alexandrbig1 <alexsmagin1@gmail.com>
2023-12-25 09:47:12 -0600

    fix: cleaned code

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/goal-craft/",
   server: {
     port: 3000,
     open: true,
